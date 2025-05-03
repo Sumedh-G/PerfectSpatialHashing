237a0d5b48a18007752d0f993986179dd100064d
Sumedh Girish <sumedh.girish@gmail.com>
2025-05-04 01:52:46 +0530

    Can you even call this progress?

--- main.py
+++ main.py
@@ -3,21 +3,42 @@ import numpy as np
 
 def generateHashTable(points):
     n = len(points)
+    print(f"{n=}")
     m = int(sympy.nextprime(np.sqrt(n)))  #type:ignore
+    print(f"{m=}")
 
     h0 = np.array(points % m, dtype="int64")
     increment_factor = 2
     r = int(np.sqrt(n) / (increment_factor))
 
+    while True:
+        h1 = np.array(points % r, dtype="int64")
+        if len(np.unique(np.array([h0.T, h1.T]).T, axis=0)) != len(np.array([h0.T, h1.T]).T):
+            r *= increment_factor
+        else:
+            break
+    print(f"{r=}")
+    phashes = np.unique(np.array([h1.T, h0.T]).T, axis=0)
+
+    groups = {}
+    for h, p in phashes:
+        if tuple(h) in groups:
+            groups[tuple(h)].append(p)
+        else:
+            groups[tuple(h)] = [p]
+    points = sorted([[np.array(x), np.array(k)] for k, x in groups.items()], key=lambda x:len(x[0]), reverse=True)
+
     h = np.zeros(shape=(m, m, 2), dtype="int64") - 1
-    h1 = np.array(points % r, dtype="int64")
     phi = np.zeros(shape=(r, r, 2), dtype="int64")
-
-
-    print(f"{points=}\n{n=}\n{m=}\n{r=}\n{h0=}\n{h1=}\n{priorityList=}\n{offsets=}\n{combined_map=}\n{h=}")
+    accomodated = False
+    while not accomodated:
+        for group in points:
+            for index in group[0] + phi[group[1][1]][group[1][0]]:
+                print(index)
+        break
 
 
 if __name__ == "__main__":
     generateHashTable(
-        np.array([[2, 3], [5, 18], [13, 41], [13,42], [14, 42], [13, 43]])
+        np.random.randint(0, 101, size=(50, 2))
     )
